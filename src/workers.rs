595543b7685f879b13140eafd2ab7abc5d2a4685
Tommy René Sætre <tommy.saetre@hotmail.com>
2022-01-25 14:15:51 +0100

    Removed use of traits

--- src/workers.rs
+++ src/workers.rs
@@ -4,10 +4,28 @@ struct Workers {
     threads: Vec<thread>
 }
 
-trait Workers {
-    fn start(&self);
-    fn post(&self, fn);
-    fn post_timeout(&self, fn, i32);
-    fn join(&self);
-    fn stop(&self);
+impl Workers {
+    fn new(i32) -> self {
+
+    }
+
+    fn start(&self) {
+
+    }
+
+    fn post(&self, fn) {
+
+    }
+
+    fn post_timeout(&self, fn, i32) {
+
+    }
+
+    fn join(&self) {
+
+    }
+
+    fn stop(&self) {
+        
+    }
 }
