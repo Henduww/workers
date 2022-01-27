04d6e7b62784fb2d2593d052a5826de2f1d413b6
Tommy René Sætre <tommy.saetre@hotmail.com>
2022-01-27 15:33:51 +0100

    Created basis for creating a threadpool for running jobs with or without a timeout

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,15 @@
+mod workers;
+
 fn main() {
+    let workers = workers::Workers::new(1);
+
+    workers.post(|| {
+        println!("Regular post");
+    });
+
+    workers.post_timeout(|| {
+        println!("Post timeout 3s")
+    }, 3000);
+
+    workers.join();
 }
--- src/workers.rs
+++ src/workers.rs
@@ -1,31 +1,68 @@
 use std::thread;
+use std::sync::{Arc, Mutex, mpsc};
 
-struct Workers {
-    threads: Vec<thread>
+pub struct Workers {
+    num_workers: usize,
+    num_active_workers: usize,
+    threads: Vec<thread::JoinHandle<Arc<Mutex<mpsc::Receiver<()>>>>>,
+    sender: mpsc::Sender<Job>,
 }
 
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
 impl Workers {
-    fn new(i32) -> self {
+    pub fn new(num_workers: usize) -> Self {
+        let mut threads = Vec::with_capacity(num_workers);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let sender: mpsc::Sender<Job> = sender;
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for _ in 0..(num_workers) {
+            let thread_receiver = Arc::clone(&receiver);
+            threads.push(thread::spawn(move || loop {
+                let job = thread_receiver.lock().unwrap().recv().unwrap();
+
+                job();
+            }));
+        }
+
+        Workers {
+            num_workers: num_workers,
+            num_active_workers: 0,
+            threads: threads,
+            sender: sender,
+        }
     }
 
-    fn start(&self) {
-
-    }
-
-    fn post(&self, fn) {
-
-    }
-
-    fn post_timeout(&self, fn, i32) {
-
-    }
-
-    fn join(&self) {
-
-    }
-
-    fn stop(&self) {
+    pub fn start(&self) {
         
+    }
+
+    pub fn post<F>(&self, f: F)
+        where F: FnOnce() + Send + 'static
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+    
+    pub fn join(self) {
+        self.threads.into_iter().for_each(|thread| {
+            thread.join();
+        });
+    }
+
+    pub fn post_timeout<F>(&self, f: F, timeout: u32)
+        where F: FnOnce() + Send + 'static
+    {
+        self.post(move || {
+            thread::sleep_ms(timeout);
+            f();
+        });
+    }
+
+    pub fn stop(&self) {
+
     }
 }
